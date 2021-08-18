8154a926727814e1fb59fbc88e65cc1650e0c4dd
Eran Davidovich <erandav@google.com>
2021-08-18 19:27:00 +0300

    nvim: bufferline change tab highlights
    also add PyScaffold

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -8,14 +8,16 @@ require('bufferline').setup {
     show_buffer_icons = false,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    show_tab_indicators = false,
     separator_style = {"", ""},
     max_prefix_length = 20,
     tab_size = 25,
+    always_show_bufferline = true,
     separator_style = "thick",
   },
   -- tokyonight background
   highlights = {
     fill = {guibg = colors.bg},
+    background = {guibg = colors.bg},
+    buffer_selected = {guibg = colors.bg_highlight, guifg = colors.orange},
   }
 }

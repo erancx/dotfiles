9221a7928a7ddebadbd002938ff3d508b28ba254
Eran Davidovich <erandav@google.com>
2021-08-29 20:37:49 +0300

    nvim: fix telescope missing defaults

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,16 @@
 -- Telescope
 require('telescope').setup{
-    file_ignore_patterns = {".git/"},
-    initial_mode = "insert",
-    extensions = {
-        fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = false, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case" -- or "ignore_case" or "respect_case"
-        },
+    defaults = {
+        prompt_prefix = "   ",
+        file_ignore_patterns = {".git/"},
+        extensions = {
+            fzf = {
+                fuzzy = true, -- false will only do exact matching
+                override_generic_sorter = false, -- override the generic sorter
+                override_file_sorter = true, -- override the file sorter
+                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+            },
+        }
     }
 }
 require("telescope").load_extension("fzf")

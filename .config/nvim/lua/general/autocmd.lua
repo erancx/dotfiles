d677450a3ea6d9f5b0b29a137b5fa4f1238f41ad
Eran Davidovich <edavidovich@jumptrading.com>
2021-11-08 23:08:28 +0200

    small changes to settings.lua

--- .config/nvim/lua/general/autocmd.lua
+++ .config/nvim/lua/general/autocmd.lua
@@ -1,22 +1,32 @@
-ToggleMouse = function()
-	if vim.o.mouse == "a" then
-		vim.wo.signcolumn = "no"
-		vim.o.mouse = "v"
-		vim.wo.number = false
-	else
-		vim.wo.signcolumn = "yes"
-		vim.o.mouse = "a"
-		vim.wo.number = true
-	end
-end
+local exec = vim.api.nvim_exec -- execute Vimscript
+local cmd  = vim.cmd -- execute Vim commands
 
--- Highlight on yank
-vim.api.nvim_exec(
-	[[
+-- 2 spaces for selected filetypes
+cmd([[ autocmd FileType xml,html,xhtml,css,scssjavascript,lua,dart setlocal shiftwidth=2 tabstop=2 ]])
+
+-- json
+cmd([[ au BufEnter *.json set ai expandtab shiftwidth=2 tabstop=2 sta fo=croql ]])
+
+-- don't auto commenting new lines
+cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+
+-- remove whitespace on save
+cmd([[au BufWritePre * :%s/\s\+$//e]])
+
+-- jump to the last position when reopening a file
+cmd([[
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
+endif
+]])
+
+-- highlight on yank
+exec(
+    [[
   augroup YankHighlight
     autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=500, on_visual=true}
   augroup end
 ]],
-	false
+    false
 )
--- .config/nvim/lua/general/settings.lua
+++ .config/nvim/lua/general/settings.lua
@@ -10,10 +10,39 @@ require("onedark").setup({
     functionStyle = "italic",
 })
 
-cmd('autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=grey') --to Show whitespace, MUST be inserted BEFORE the colorscheme command
+set.autoindent      = true          -- maintain indent of current line
+set.backspace       = 'indent,start,eol' -- make backspace behave like normal again
+set.completeopt     = 'menuone,noselect,noinsert' -- completion options
+set.cursorline      = true 	        -- highlight current line
+set.expandtab       = false         -- don't expand tabs into spaces
 set.guifont		      = 'DroidSansMono Nerd Font 11'
-set.termguicolors   = true      -- Enable GUI colors for the terminal to get truecolor
+set.hidden          = true      -- allows you to hide buffers with unsaved changes without being prompted
+set.hlsearch        = true 	        -- highlighted search results
+set.ignorecase      = true 	        -- ignore case sensetive while searching
+set.inccommand      = 'split'   -- live preview of :s results
+set.incsearch       = true 	        -- incremental search
+set.laststatus      = 2         -- always show status line
+set.lazyredraw      = true -- faster scrolling
 set.list            = false      -- show whitespace
+set.number          = true          -- show line numbers
+set.scrolloff       = 1             -- when scrolling, keep cursor 1 lines away from screen border
+set.shell           = 'zsh'     -- shell to use for `!`, `:!`, `system()` etc.
+set.shiftround      = true
+set.shiftwidth      = 2             -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
+set.showmatch       = true 	        -- show the matching part of the pair for [] {} and ()
+set.sidescrolloff   = 2             -- keep 30 columns visible left and right of the cursor at all times
+set.smartcase       = true
+set.smarttab        = true          -- <tab>/<BS> indent/dedent in leading whitespace
+set.softtabstop     = 2
+set.splitbelow      = true      -- open horizontal splits below current window
+set.splitright      = true      -- open vertical splits to the right of the current window
+set.tabstop         = 2             -- spaces per tab
+set.termguicolors   = true      -- Enable GUI colors for the terminal to get truecolor
+set.undodir         = os.getenv("HOME") .. "/.vim" -- vim.options do not expand "~"
+set.undofile        = true
+set.updatetime      = 250              -- CursorHold interval
+set.wildignore      = set.wildignore + '*/tmp/*,*.so,*.swp,*.zip,*.rpm,*.o,*.obj,.git,*.rbc,*.pyc,__pycache__' -- patterns to ignore during file-navigation
+set.wrap            = false         -- don't automatically wrap on load
 set.listchars = {
          nbsp       = '⦸',      -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
          extends    = '»',      -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
@@ -28,68 +57,7 @@ set.fillchars = {
         fold        = '·',      -- MIDDLE DOT (U+00B7, UTF-8: C2 B7)
         vert        = ' ',      -- remove ugly vertical lines on window division
 }
-set.wrap            = false         -- don't automatically wrap on load
-set.showmatch       = true 	        -- show the matching part of the pair for [] {} and ()
-set.cursorline      = true 	        -- highlight current line
-set.number          = true          -- show line numbers
-set.incsearch       = true 	        -- incremental search
-set.hlsearch        = true 	        -- highlighted search results
-set.ignorecase      = true 	        -- ignore case sensetive while searching
-set.smartcase       = true
-set.scrolloff       = 1             -- when scrolling, keep cursor 1 lines away from screen border
-set.sidescrolloff   = 2             -- keep 30 columns visible left and right of the cursor at all times
-set.backspace       = 'indent,start,eol' -- make backspace behave like normal again
-set.updatetime      = 250              -- CursorHold interval
-set.softtabstop     = 2
-set.softtabstop     = 2
-set.shiftwidth      = 2             -- spaces per tab (when shifting), when using the >> or << commands, shift lines by 4 spaces
-set.tabstop         = 2             -- spaces per tab
-set.smarttab        = true          -- <tab>/<BS> indent/dedent in leading whitespace
-set.autoindent      = true          -- maintain indent of current line
-set.expandtab       = false         -- don't expand tabs into spaces
-set.shiftround      = true
-set.splitbelow      = true      -- open horizontal splits below current window
-set.splitright      = true      -- open vertical splits to the right of the current window
-set.laststatus      = 2         -- always show status line
-set.undofile        = true
-set.undodir         = os.getenv("HOME") .. "/.vim" -- vim.options do not expand "~"
-set.hidden          = true      -- allows you to hide buffers with unsaved changes without being prompted
-set.inccommand      = 'split'   -- live preview of :s results
-set.shell           = 'zsh'     -- shell to use for `!`, `:!`, `system()` etc.
 --set.colorcolumn = "79"        -- vertical word limit line
-
--- highlight on yank
-exec([[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=500, on_visual=true}
-  augroup end
-]], false)
-
--- jump to the last position when reopening a file
-cmd([[
-if has("autocmd")
-  au BufReadPost * if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
-endif
-]])
-
--- patterns to ignore during file-navigation
-set.wildignore  = set.wildignore + '*.o,*.rej,*.so'
--- remove whitespace on save
-cmd([[au BufWritePre * :%s/\s\+$//e]])
--- faster scrolling
-set.lazyredraw = true
--- don't auto commenting new lines
-cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
--- completion options
-set.completeopt = 'menuone,noselect,noinsert'
-
-
-
--- 2 spaces for selected filetypes
-cmd([[ autocmd FileType xml,html,xhtml,css,scssjavascript,lua,dart setlocal shiftwidth=2 tabstop=2 ]])
--- json
-cmd([[ au BufEnter *.json set ai expandtab shiftwidth=2 tabstop=2 sta fo=croql ]])
 
 local disabled_built_ins = {
     "2html_plugin",

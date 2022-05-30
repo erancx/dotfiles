04dadd7ecb2e312ce8c4d99430141e1a26f0b7e1
Eran Davidovich <edavidovich@jumptrading.com>
2022-05-30 17:25:15 +0300

    nvim: remove unused function

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,6 @@
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
-local check_backspace = function()
-    local col = vim.fn.col(".") - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-end
-
 --   פּ ﯟ   some other good icons
 local kind_icons = {
     Text = "",
